2097619c0efe09e0f3fca3c92959a2199e003373
Tanya Phung <10180091+tanyaphung@users.noreply.github.com>
2019-01-25 21:09:49 -0500

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# User modifiable variables
+
+export PSHOME="/home/tphung3/scratch/polysolver"
+export SAMTOOLS_DIR="/home/tphung3/softwares/miniconda3/envs/polysolver/bin"
+export JAVA_DIR="/home/tphung3/softwares/miniconda3/envs/polysolver/bin"
+export NOVOALIGN_DIR="/home/tphung3/softwares/miniconda3/pkgs/novoalign-3.06.05-0/bin"
+export GATK_DIR="/home/tphung3/softwares/picard-tools-1.120/"
+export TMP_DIR="/scratch/tphung3/tmp"
+export NUM_THREADS=8
